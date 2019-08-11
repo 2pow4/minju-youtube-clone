fb143067ec7bc577eaeb18c523398fddd5798557
kmju1997 <kmju1997@gmail.com>
2019-08-11 13:30:03 +0900

    <feat>: implement youtube search and play

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,33 +1,81 @@
 import React from "react";
+import axios from "axios";
 import Search from "./Search";
 import PlayList from "./PlayList";
+import Player from "./Player"
 
 class App extends React.Component{
     constructor(props){
         super(props)
         this.state = {
             keyword: '',
-            results: []
+            results: [],
+            videoId: undefined,
+            playerEnable: false
         }
     }
 
-    ComponentWillUpdate(){}
+    // componentDidUpdate(PrevProps, PrevState){
+    //     if (PrevState.keyword === this.state.keyword){
+    //         return
+    //     }
+    //     else{
+    //         this.searchByKeyWord()
+    //     }
+    // }
 
-    handleSearch = (value) => {
-        console.log("change keyword to" + value)
-        this.setState({keyword : value})
+    searchByKeyWord = () => {
+        console.log("did updated")
+        axios.get('https://www.googleapis.com/youtube/v3/search',
+        {
+            params:{
+                key: config.YOUTUBE_API_KEY,
+                part: 'snippet',
+                type: 'video',
+                q: this.state.keyword
+            }
+        
+        }).then(
+            response => {
+                console.log(response.data.items)
+                this.setState({results: response.data.items})}
+        )
     }
 
+    handleSearch = (value) => {
+        console.log("change keyword to " + value)
+        this.setState({keyword : value}, this.searchByKeyWord)
+    }
+
+    handlePlay = (videoId) => {
+        this.setState({videoId: videoId, playerEnable: true})
+    }
+
+    handleStop = () =>{
+        this.setState({playerEnable: false})
+    }
+    // or just redirect
+
+
     render(){
+        const playlist = this.state.results.map((result, index) => {
+            const snippet = result.snippet
+            return (
+                <PlayList key={index} videoId={result.id.videoId} title={snippet.title} thumbnail = {snippet.thumbnails.default.url} 
+                channelTitle = {snippet.channelTitle} handleClick={this.handlePlay} />
+            )
+        })
+
+        const showView =  this.state.playerEnable ? <Player videoId={this.state.videoId}/> : playlist
+        
         return(
             <div>
+                <div>
+                    <span onClick={this.handleStop} style={{visibility : this.state.playerEnable ? 'visible': 'hidden'}}> 뒤로가기</span>
+                    <a href="#"><div id="banner"> Youtube Search & Play </div></a>                   
+                </div>
                 <Search handleSearch={this.handleSearch}/>
-                {this.state.results.map((result) => {
-                    return (
-                        //todo: arrange data for PlayList
-                        <PlayList result={result}/>
-                    )
-                })}
+                {showView}    
             </div>
         )
     }
