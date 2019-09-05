9f964843d5f98efefd63b836e14ec49cf6dfe18e
kmju1997 <kmju1997@gmail.com>
2019-09-06 03:34:44 +0900

    <fix>(redux): rename search as searcher

--- src/jsx/components/Searcher.jsx
+++ src/jsx/components/Searcher.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react'
 
-const Search = (props) => { 
+const Searcher = (props) => { 
         return (
             <div>
-                <input name='keyword' value={props.value} onChange={props.handleChange}>
+                <input name='keyword' value={props.keyword} onChange={props.handleChange}>
                 </input>
                 <button className='button' onClick={props.handleSearch}>Search</button>
             </div>
         )
     }
 
-export default Search;
+export default Searcher;
